d01e900201ef0672f555cb0873278dd3399266f7
Danielle Marie <dhutchings36@yahoo.com>
2019-08-22 20:27:40 -0500

    added all files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import HelloWorld from './state-drills/HelloWorld.js';
+//import Bomb from './state-drills/Bomb.js';
+import RouletteGun from './state-drills/RouletteGun.js';
+
+
+ReactDOM.render(<RouletteGun />, document.getElementById('root'));
+
---
+++ src/state/Tabs.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Tabs extends React.Component {
+  static defaultProps = { tabs: [] };
+  render() {
+    const buttons = this.props.tabs.map((tab, index) => (
+      <button key={index}>
+        {tab.name}
+      </button>
+    ))
+    const currentTab = this.props.tabs[0]
+    return (
+      <div>
+        {buttons}
+      </div>
+    )
+  }
+}
+
+export default Tabs;
