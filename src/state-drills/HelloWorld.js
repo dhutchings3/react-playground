eec8fff227d5dbf7a12b779b2d7340b039c9e01a
Danielle Marie <dhutchings36@yahoo.com>
2019-08-24 10:03:45 -0500

    added new files and made changes to old files

--- src/state-drills/HelloWorld.js
+++ src/state-drills/HelloWorld.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+//import React, {Component} from 'react';
 
-export default class HelloWorld extends Component {
+//export default class HelloWorld extends Component {
     state = {
         who: 'world',
     };
--- src/state/Tabs.test.js
+++ src/state/Tabs.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Tabs from './Tabs';
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import {tabsProp} from './App.js'
 
 describe(`Tabs Component`, () => {
   it('renders without errors', () => {
@@ -8,4 +11,21 @@ describe(`Tabs Component`, () => {
     ReactDOM.render(<Tabs />, div);
     ReactDOM.unmountComponentAtNode(div);
   })
+
+  it('renders empty given no tabs', () => {
+    const wrapper = shallow(<Tabs />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('renders the first tab by default', () => {
+    const wrapper = shallow(<Tabs tabs={tabsProp} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('closes the first tab and opens any clicked tab', () => {
+    const wrapper = shallow(<Tabs tabs={tabsProp} />)
+    wrapper.find('button').at(1).simulate('click')
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
 })
